2a1a28b577ed7686e3e525d3939347a275b86531
benybenmo <benybenmo@gmail.com>
2023-11-15 07:05:38 -0800

    update puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,13 @@
-#!/usr/bin/env bash
-# using puppet to make changes to my configuration file
-
-file { 'ect/ssh/ssh_cofig':
-	ensure => present,
-
-content =>"
-
-	#SSH client configuration
-	host*
-	Identityfile ~/.ssh/school
-	PasswordAuthentication no
-	",
-
+# Client configuration
+exec { 'Turn off passwd auth':
+  command => 'bash -c "echo PasswordAuthentication no >> /etc/ssh/ssh_config"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
+exec { 'Declare identity file':
+  command => 'bash -c "echo IdentityFile \'~/.ssh/school\' >> /etc/ssh/ssh_config"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
+exec { 'Turn on pubkey auth':
+  command => 'bash -c "echo PubkeyAuthentication yes >> /etc/ssh/ssh_config"',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
